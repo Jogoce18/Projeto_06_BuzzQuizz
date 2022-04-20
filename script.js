359d75f5fb9412e7e473d60e6401e4f5ddeab2ef
Jogoce18 <gomezjohana655@gmail.com>
2022-04-20 16:51:06 -0500

    Added Layout Desktop

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function abrirMenu() {
+    document.querySelector(".criarquiz").classList.add("hidden");
+    document.querySelector(".conteiner").classList.remove("hidden");
+    console.log("ola mundo")
+}
+function abrirMenu1() {
+    document.querySelector(".conteiner").classList.add("hidden");
+    document.querySelector(".conteiner1").classList.remove("hidden");
+    console.log("ola mundo")
+}
+function abrirMenu2() {
+    document.querySelector(".conteiner1").classList.add("hidden");
+    document.querySelector(".conteiner2").classList.remove("hidden");
+    console.log("ola mundo")
+}
+function abrirMenu3() {
+    document.querySelector(".conteiner2").classList.add("hidden");
+    document.querySelector(".conteiner3").classList.remove("hidden");
+    console.log("ola mundo")
+}
